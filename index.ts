53ad7449ed67d31b58a0ad1b83d55e4abc956e91
Jackson Jessup <jessupjn@umich.edu>
2025-04-30 11:20:15 -0400

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import { web3 } from "@coral-xyz/anchor";
 import { AnchorUtils, asV0Tx, PullFeed } from "@switchboard-xyz/on-demand";
 
 const feeds = [
-  new web3.PublicKey("4ieXFeAqWwLaySNYqS8uGgpUCdhWUVjXRFWPE6dmZNBU"),
+  new web3.PublicKey("G4FdLzuezfaJxBd8eChuw1NU4Sq3n1rasGTwSh7dXegN"),
+  new web3.PublicKey("CLjvwowzQ2L9PrmXA6zqbamxLVeDY9vE87aBxMZLJLoY"),
+  new web3.PublicKey("DPvVSQYhZXQ2ygfT2Qjdg6iyeQVAyiz8okj88YRjy6NN"),
+  new web3.PublicKey("Fqv8vT5fdjvBbHd5k4B4ZvnXLH6bbdKP8cMv93ybCP8W"),
 ];
 
 (async () => {
@@ -17,15 +20,13 @@ const feeds = [
   console.log();
   console.log("Fetching update for feeds: ", JSON.stringify(feeds, null, 2));
 
-  const [instructions, luts, rawResponse] = await PullFeed.fetchUpdateManyIx(
-    sbProgram,
-    {
+  const [instructions, luts, rawResponse] =
+    await PullFeed.fetchUpdateManyLightIx(sbProgram, {
       feeds: feeds,
       chain: "solana",
       network: "mainnet-beta",
       numSignatures: 2,
-    }
-  );
+    });
   console.log();
   console.log("Retrieved response from oracles:");
   console.log(rawResponse);
